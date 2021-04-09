c264ab33fe29d1433b2c68d8502a0cf914e9a50d
dianaki <diana.kuramshina21@gmail.com>
2021-04-09 18:56:53 +0300

    create tablet and mobile version

--- script/main.js
+++ script/main.js
@@ -1,7 +1,25 @@
 const channelSlider = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
 
   // Navigation arrows
   navigation: {
@@ -14,7 +32,19 @@ const channelSlider = new Swiper('.channel-slider', {
 const recommendlSlider = new Swiper('.recommend-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1000: {
+      slidesPerView: 2
+    },
+    800: {
+      slidesPerView: 1
+    }
+  },
 
   // Navigation arrows
   navigation: {
@@ -26,11 +56,36 @@ const recommendlSlider = new Swiper('.recommend-slider', {
 const recChannelSlider = new Swiper('.rec-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
 
   // Navigation arrows
   navigation: {
     nextEl: '.rec-button-next',
     prevEl: '.rec-button-prev',
   },
+});
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
 });
